e0c04e50fba0ee98ac72ad5ba41613a9dad9ff26
Christian Evaroa <evaroachri@l-02-320.mddn.vuw.ac.nz>
2014-05-30 11:31:25 +1200

    hello

--- web.js
+++ web.js
@@ -95,7 +95,7 @@ app.get('/nearest/:mylat/:mylon', function(req, res) {
  * Get list of monsters the user has caught
  */
  app.get('/mymonsters/:uid', function(res, res) {
-  client.query('SELECT * FROM monsterdex WHERE userid = '+res.body.uid), function(err, result) {
+  client.query('SELECT * FROM monsterdex WHERE userid = '+res.body.uid, function(err, result) {
     res.send(result.rows);
   });
  });
