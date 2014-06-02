96e545e8bdf607ad7d4600e53852db30e913c065
chrisrgibb <chrisrgibb@gmail.com>
2014-06-02 15:03:25 +1200

    fixed another typo grrr~

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,26 @@
 
-module.exports = function(app, client){
+module.exports = function(app, client, passport){
 
 /*
  *  routes to pages
  */
+
+
+ 	app.get('/signin', function(req, res){
+		res.sendfile('/views/admin-signin.html', {root :__dirname });
+	});
+
+	app.post('/signin', 
+		passport.authenticate('local-signin', {session :false }),
+		function(req, res) {
+			console.log('user ' );
+
+			var jsonData = { id: req.user.id, name: req.user.name };
+			console.log(jsonData);
+			res.send(jsonData);
+	});
+
+
 
 	app.get('/', function(req, res) {
 	  res.sendfile('/views/index.html', {root :__dirname });
--- web.js
+++ web.js
@@ -23,9 +23,9 @@ app.configure(function(){
   
 });
 
-require('./app/routes')(app, client); // put the routes in here. can change them later if we need. 
 require('./app/passport')(passport, client);
-require('./app/passport-routes.js')(app, passport);
+require('./app/routes')(app, client, passport); // put the routes in here. can change them later if we need. 
+// require('./app/passport-routes.js')(app, passport);
 
 var lat;
 var lon;
