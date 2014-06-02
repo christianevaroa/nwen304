c6a7c4743b43daa6c1e8e54088d9448999ea70d0
chrisrgibb <chrisrgibb@gmail.com>
2014-06-02 14:28:06 +1200

    added require passport in web.js

--- app/passport-routes.js
+++ app/passport-routes.js
@@ -6,11 +6,15 @@ module.exports = function(app, passport){
 		res.sendfile('/view/admin-signin.html');
 	});
 
+	app.post('/signin', passport.authenticate('local-signin', {session :false }),
+		function(req, res) {
+			console.log('user ' );
 
-
-
-
-
+			var jsonData = { id: req.user.id, username: req.user.username };
+			console.log(jsonData);
+			res.send(jsonData);
+	});
+	
 }
 
 
