01000c0a2fc0e256a4bee210c250c173cfd4da2d
christianevaroa <christianevaroa@gmail.com>
2014-06-05 15:19:49 +1200

    think i fixed it

--- app/routes.js
+++ app/routes.js
@@ -120,7 +120,7 @@ module.exports = function(app, client, passport){
 	 * Get list of monsters the user has caught
 	 */
 	 app.get('/mymonsters/:uid', function(req, res) {
-	  client.query('SELECT * FROM monsterdex WHERE userid = '+req.body.uid, function(err, result) {
+	  client.query('SELECT * FROM monsterdex WHERE userid = '+req.params.uid, function(err, result) {
 	    res.send(result.rows);
 	  });
 	 });
